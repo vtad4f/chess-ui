1a84e4d0d3d39928773b5d0231ec9af5d0d72ff3
Vincent Allen <vtadf@mst.edu>
2019-09-01 19:14:33 +0100

    Significant refactoring
    Detects checkmate

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ import chess.svg
 from PyQt5.QtCore import pyqtSlot, Qt
 from PyQt5.QtSvg import QSvgWidget
 from PyQt5.QtWidgets import QApplication, QWidget
+import sys
 
 
-class MainWindow(QWidget):
+class ChessBoard(QWidget):
    """
-   Create a surface for the chessboard.
+      BRIEF  An interactive chessboard that only allows legal moves
    """
    
    def __init__(self):
@@ -20,21 +21,20 @@ class MainWindow(QWidget):
       Initialize the chessboard.
       """
       super().__init__()
-
-      self.setWindowTitle("Chess Titan")
+      
       self.setGeometry(300, 300, 800, 800)
-
-      self.widgetSvg = QSvgWidget(parent=self)
-      self.svgX = 50                    # top left x-pos of chessboard
-      self.svgY = 50                    # top left y-pos of chessboard
-      self.cbSize = 600                  # size of chessboard
-      self.widgetSvg.setGeometry(self.svgX,self.svgY, self.cbSize, self.cbSize)
-      self.coordinates = True
-      # see chess.svg.py line 129
-      self.margin = 0.05*self.cbSize if self.coordinates == True else 0
-      self.squareSize  = (self.cbSize - 2 * self.margin) / 8.0
+      
+      self.svg_widget = QSvgWidget(parent=self)
+      self.svg_x = 50 # top left x-pos of chessboard
+      self.svg_y = 50 # top left y-pos of chessboard
+      self.board_size = 600 # size of chessboard
+      self.svg_widget.setGeometry(self.svg_x, self.svg_y, self.board_size, self.board_size)
+      
+      self.margin = 0.05 * self.board_size
+      self.square_size  = (self.board_size - 2 * self.margin) / 8.0
+      
+      self.last_clicked = None
       self.board = chess.Board()
-      self.pieceToMove = [None, None]
       self.DrawBoard()
       
    @pyqtSlot(QWidget)
@@ -46,38 +46,60 @@ class MainWindow(QWidget):
       Moves must be made according to the rules of chess because
       illegal moves are suppressed.
       """
-      if self.svgX < event.x() <= self.svgX + self.cbSize and self.svgY < event.y() <= self.svgY + self.cbSize:   # mouse on chessboard
-         if event.buttons() == Qt.LeftButton:
-            # if the click is on chessBoard only
-            if self.svgX + self.margin < event.x() < self.svgX + self.cbSize - self.margin and self.svgY + self.margin < event.y() < self.svgY + self.cbSize - self.margin:
-               file = int((event.x() - (self.svgX + self.margin))/self.squareSize)          
-               rank = 7 - int((event.y() - (self.svgY + self.margin))/self.squareSize) 
-               square = chess.square(file, rank)                  # chess.sqare.mirror() if white is on top
-               piece = self.board.piece_at(square)
-               coordinates = '{}{}'.format(chr(file + 97), str(rank +1))
-               if self.pieceToMove[0] is not None:
-                  move = chess.Move.from_uci('{}{}'.format(self.pieceToMove[1], coordinates))
-                  if move in self.board.legal_moves:
-                     self.board.push(move)
-                     print(self.board.fen())
-                  piece = None
-                  coordinates= None
-               self.pieceToMove = [piece, coordinates]
+      if self.LeftClickedBoard(event):
+         clicked_piece, clicked_algebraic = self.GetClicked(event)
+         
+         if self.last_clicked:
+            last_piece, last_algebraic = self.last_clicked
+            move = chess.Move.from_uci(last_algebraic + clicked_algebraic)
+            if move in self.board.legal_moves:
+               self.board.push(move)
                self.DrawBoard()
-               
+            self.last_clicked = None
+            
+         self.last_clicked = (clicked_piece, clicked_algebraic)
+         
+   def GetClicked(self, event):
+      """
+      """
+      file      =     int((event.x() - (self.svg_x + self.margin))/self.square_size)
+      rank      = 7 - int((event.y() - (self.svg_y + self.margin))/self.square_size)
+      square    = chess.square(file, rank) # TODO - chess.sqare.mirror() if white is on top
+      piece     = self.board.piece_at(square)
+      algebraic = chr(file + 97) + str(rank + 1)
+      return piece, algebraic
+      
+   def LeftClickedBoard(self, event):
+      """
+      """
+      return all([
+         event.buttons() == Qt.LeftButton,
+         self.svg_x               < event.x() <= self.svg_x + self.board_size,
+         self.svg_y               < event.y() <= self.svg_y + self.board_size,
+         self.svg_x + self.margin < event.x() <  self.svg_x + self.board_size - self.margin,
+         self.svg_y + self.margin < event.y() <  self.svg_y + self.board_size - self.margin,
+      ])
+      
    def DrawBoard(self):
       """
          BRIEF  Redraw the chessboard based on self.board state
+                Highlight src and dest squares for last move
+                Highlight king if in check
       """
-      self.widgetSvg.load(self.board._repr_svg_().encode("UTF-8"))
+      self.svg_widget.load(self.board._repr_svg_().encode("UTF-8"))
+      print(self.board.fen())
+      if self.board.is_game_over():
+         print("Game over!")
+      sys.stdout.flush()
       
       
 if __name__ == "__main__":
    """
    """
-   chessTitan = QApplication([])
-   window = MainWindow()
-   window.show()
-   chessTitan.exec()
+   q_app = QApplication([])
+   wnd = ChessBoard()
+   wnd.setWindowTitle("Chess")
+   wnd.show()
+   q_app.exec()
    
    
